746b510ed95f251076064079ce05baa100ba5e22
gargriya01 <72300799+gargriya01@users.noreply.github.com>
2023-09-07 13:30:03 +0530

    "TILL ERRORBOUNDARY DONE"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ComponentA from "./Components/ComponentA.js";
+import ComponentB from "./Components/ComponentB.js";
+import ErrorBoundary from "./Components/ErrorBoundary.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <ErrorBoundary>
+          <ComponentA />
+        </ErrorBoundary>
+        <ErrorBoundary>
+          <ComponentB />
+        </ErrorBoundary>
+      </>
+    );
+  }
 }
 
 export default App;
